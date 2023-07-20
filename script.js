92e95994cffa8d9ebcaeeaa7386a7886db590a6b
parag <paragcoimbatore745@gmail.com>
2023-07-20 17:00:05 +0530

    Final change 1

--- script.js
+++ script.js
@@ -15,11 +15,12 @@ function resetForm() {
     photoInput.value = "";
 
     // Clear error messages
-    const errors = document.querySelectorAll(".error");
-    errors.forEach(error => error.textContent = "");
+    const errors = document.querySelectorAll(".error");  //return karega all html element with class="error"
+    errors.forEach(error => error.textContent = "");     //unn return kiye hue elements pe sabke textcontext ko empty string karega
 }
 
 function validateForm() {
+    //getting all the values from the input to variables
     const id = idInput.value;
     const name = nameInput.value;
     const age = parseInt(ageInput.value);
@@ -27,45 +28,58 @@ function validateForm() {
     const designation = designationInput.value;
     const photoUrl = photoInput.value;
 
+    //A variable to keep track of the form's validity
     let isValid = true;
 
+    //id cant be null
     if (!id) {
         document.getElementById("idError").textContent = "ID cannot be null.";
         isValid = false;
     }
 
+    //Name cant be in numbers 
     if (!name.match(/^[A-Za-z\s]+$/)) {
         document.getElementById("nameError").textContent = "Name should contain alphabets only.";
         isValid = false;
     }
 
+    //age in 18 to 60
     if (age < 18 || age > 60) {
         document.getElementById("ageError").textContent = "Age should be in the range of 18 to 60.";
         isValid = false;
     }
 
+    //gender should be added
     if (!gender) {
         document.getElementById("genderError").textContent = "Please select a gender.";
         isValid = false;
     }
 
+    //designation should be add
     if (!designation) {
         document.getElementById("designationError").textContent = "Please select a designation.";
         isValid = false;
     }
 
+    //photourl should be added compulsorily
     if (!photoUrl) {
         document.getElementById("photoError").textContent = "Self Photo URL cannot be empty.";
         isValid = false;
     }
 
+    //sab theek hai
     return isValid;
 }
 
+//id unique hona chahiye
 function isIdUnique(id) {
+    //gets all rows , not header
     const rows = table.getElementsByTagName("tr");
-    for (let i = 1; i < rows.length; i++) { // Skip the header row
+    for (let i = 1; i < rows.length; i++) { // Skip karega header row
         const cells = rows[i].getElementsByTagName("td");
+        // anything cell is means the inputs
+        //cell 0 matlab at first cell i.e id it should not be same as id 
+        //kisi bhi row k column mein id pahle jaisa same na ho
         if (cells[0].textContent === id) {
             return false;
         }
@@ -73,7 +87,9 @@ function isIdUnique(id) {
     return true;
 }
 
+
 function addUser() {
+    //agar theek se validate ho gaya toh , aage 
     if (validateForm()) {
         const id = idInput.value;
         const name = nameInput.value;
@@ -82,12 +98,16 @@ function addUser() {
         const designation = designationInput.value;
         const photoUrl = photoInput.value;
 
+        //if id is not unique , give error
         if (!isIdUnique(id)) {
             document.getElementById("idError").textContent = "ID should be unique.";
             return;
         }
 
+        //create new row 
         const newRow = table.insertRow(-1);
+        //-1 means it will be inserted at the end of row
+        //innerhtml is used to enter info in html table
         newRow.innerHTML = `
             <td>${id}</td>
             <td>${name}</td>
@@ -102,7 +122,7 @@ function addUser() {
             </td>
         `;
         resetForm();
-        scrollDown();w
+        scrollDown();
     }
 }
 
@@ -110,6 +130,7 @@ function scrollToSection(sectionId) {
     const section = document.getElementById(sectionId);
     section.scrollIntoView({ behavior: "smooth" });
 }
+
 
 function editUser(editButton) {
     const row = editButton.parentNode.parentNode;
@@ -121,6 +142,9 @@ function editUser(editButton) {
     genderInput.value = cells[3].textContent;
     designationInput.value = cells[4].textContent;
     photoInput.value = cells[5].getElementsByTagName("img")[0].getAttribute("src");
+
+
+
 }
 
 function deleteUser(deleteButton) {
@@ -149,3 +173,30 @@ function scrollDown() {
         behavior: "smooth"
     });
 }
+
+function searcher(){
+    document.addEventListener("DOMContentLoaded", function() {
+        const filterInput = document.getElementById("filterInput");
+        const dataTable = document.getElementById("dataTable");
+        const rows = dataTable.getElementsByTagName("tr");
+    
+        filterInput.addEventListener("keyup", function() {
+            const filterValue = filterInput.value.toLowerCase();
+    
+            for (let i = 1; i < rows.length; i++) { // Start from index 1 to skip the header row
+                const row = rows[i];
+                const cells = row.getElementsByTagName("td");
+    
+                // Convert the HTMLCollection to an array to use the filter() method
+                const cellTexts = Array.from(cells).map(cell => cell.textContent || cell.innerText);
+    
+                const showRow = cellTexts.some(cellText => cellText.toLowerCase().includes(filterValue));
+    
+                row.style.display = showRow ? "" : "none";
+            }
+        });
+    });
+
+}
+
+
